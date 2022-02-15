4e5c4d911945e8bb19c08de1efb06871377624a2
Esther Hoeun Yu <togom11@g.skku.edu>
2022-02-15 20:10:19 +0900

    Number and string assignment
    You learned how to create variables and assign data to them earlier.
    
    This time, let's use it by assigning numbers and strings to variables through practice.
    
    Type conversion is essential to operate on variables of different data types or to change the format in which data is expressed.
    
    Where did the type conversion take place in this lab? Take a good look!
    
    
    Let's do this!
    Take a look at the skeleton code written in the code editor, and understand how the code works.
    Try assigning values ​​to the heartbeats and name variables in lines 2 and 15 by entering a number and a string, respectively.
    Click the Run button to check the result.
    Tips
    Take a look at the print() function on the last line! Can you see the addition (+) operation between strings and strings?
    
    In addition, the heartbeats variable, which is an integer type, is converted into a string data type using the str() function and operation is being performed!

---
+++ heartbeat.py
@@ -0,0 +1,44 @@
+# On average, the heart beats 75 times a minute. Count your heartbeats for 1 minute, and assign them to the variable heartbeats below.
+heartbeats = 75
+
+
+# The heart beats 20% more or less than the average, depending on your physical condition and mood.
+# Below is the code that assigns variables and their values ​​to cases where the heart rate is fast or slow.
+
+
+# You can also use arithmetic operators directly on variables.
+fast_beats = int(heartbeats * 1.2)
+# I used the int() type conversion function to clear the decimal point.
+slow_beats = int(heartbeats * 0.8)
+
+# Assign your own name to the name variable. Don't forget the quotation marks when assigning strings to variables.
+name = "esthi"
+
+
+# Check the code below and the output result carefully.
+print("My name is " + name + ", my heart is beating " + str(heartbeats) + " times per minute.")
+print("But sometimes it beats " + str(fast_beats) + " about " + str(fast_beats) + " when I'm happy or surprised, and " + "I beat about " + str(slow_beats) + "" when I'm depressed or bored.")
+      
+      
+      
+
+# # 평균적으로 심장은 1분에 75번 뛰어요. 1분간 심장 박동을 세 보고, 아래 변수 heartbeats에 할당해 주세요.
+# heartbeats = 759
+
+
+# # 심장 박동은 몸 상태나 기분에 따라 평균보다 20% 정도 더 많이 혹은 적게 뛰기도 해요. 
+# # 아래는 심장 박동이 빠르거나 느려지는 경우에 대한 변수와 그 값을 할당한 코드예요.
+
+
+# # 변수에도 사칙 연산자를 직접 사용할 수 있어요.
+# fast_beats = int(heartbeats * 1.2) 
+# # 소수점 표시를 지우기 위해 int() 형 변환 함수를 사용했어요.
+# slow_beats = int(heartbeats * 0.8) 
+
+# # name 변수에 자신의 이름을 할당해 주세요. 변수에 문자열을 대입할 때에는 따옴표를 잊지 마세요.
+# name = "esthi"
+
+
+# # 아래 코드와 출력 결과를 잘 확인해 보세요.
+# print("내 이름은 " + name + ", 내 심장은 1분에 " + str(heartbeats) + "번 뛰고 있어.") 
+# print("하지만 기쁘거나 놀라운 일이 있을 때는 " + str(fast_beats) + "번 정도 뛰기도 하고, " + "기분이 우울하거나 심심할 때는 " + str(slow_beats) + "번 정도 뛰기도 해.")
