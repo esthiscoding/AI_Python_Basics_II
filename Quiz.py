f2179a011834d3b4c81c9005a57b52128a500a88
Esther Hoeun Yu <togom11@g.skku.edu>
2022-02-15 19:56:50 +0900

    Create Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,19 @@
+#'result'라는 변수를 선언하고, 숫자 23571를 할당해 주세요.
+result = 23571
+
+#'divided'라는 변수를 선언하고, result 변수에 1024를 곱한 값을 대입해 주세요.
+divided = result*1024
+
+#'multiplied'이라는 변수를 하나 선언하고, divided 변수를 243로 나눈 값을 저장해 주세요.
+multiplied = divided/243
+
+#'subtracted'이라는 변수를 하나 선언하고, multiplied 변수에 4927819을 더한 값을 저장해 주세요.
+subtracted = multiplied+4927819
+
+#'get_go'라는 변수를 하나 선언하고, subtracted 변수에서 829176을 뺀 값을 저장해 주세요.
+get_go = subtracted-829176
+
+# 거꾸로 되짚어서 계산한 원래의 숫자는 얼마일까요?
+print('"캐터필러, 네가 생각한 답은', get_go, '원이야!"') 
+
+# get_go = (((23571*1024) /243) + 4927819)-829176
